165038134cf6fe1222a25407167b22ada148923d
Abhishek Brahmachary <abhishekb@nihilentanalytics.com>
2019-09-27 12:27:00 +0530

    second

---
+++ actions/consumeevents.py
@@ -0,0 +1,8 @@
+import sys
+from st2common.runners.base_action import Action
+import requests
+class MyAction(Action):
+    def run(self):
+        reqmsg = requests.get('https://github.com/timeline.json')
+        print(reqmsg.json())
+        return(True,reqmsg)
